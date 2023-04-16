b24e3fbbac939998abad638f8f5e1f1e2782e432
vera4056 <103599166+vera4056@users.noreply.github.com>
2023-04-16 17:06:52 +0300

    arrays modified

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,16 @@
 function compareArrays(arr1, arr2) {
+    return arr1.length === arr2.length && arr1.every((elem, index) => elem === arr2[index]);
   
 }
 
 function getUsersNamesInAgeRange(users, gender) {
+    
+    let result = users.filter((user) => user.gender === gender)
+                      .map((user) => user.age)
+                      .reduce((acc, age) => acc + age, 0)
+                / (users.filter((user) => user.gender === gender).length || 1);
+
+     return result;
+
   
 }
