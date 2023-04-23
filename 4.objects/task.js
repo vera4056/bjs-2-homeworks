09d2beeb4fe1f0c309dd0fa6240c78f6fde42104
vera4056 <103599166+vera4056@users.noreply.github.com>
2023-04-23 15:23:49 +0300

    homework added

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,32 @@
-function Student(name, gender, age) {
-  
+"use strict";
+
+function Student(name, gender, age){
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+    this.marks = this.marks.concat(marksToAdd);
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (!this.marks || this.marks.length == 0) {
+        return 0;
+    }
+
+    let average = this.marks.reduce((acc, mark) => acc + mark, 0) / this.marks.length;
+
+    return average;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.exclude = reason;
 }
