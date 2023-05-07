175fd31aab9014abab7d35ab25f718509fd39b25
vera4056 <103599166+vera4056@users.noreply.github.com>
2023-05-07 15:54:39 +0300

    modiffied

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,30 @@
-﻿
+﻿class Triangle {
+    constructor(a, b, c) {
+        if ( a + b < c || b + c < a || c + a < b ) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+
+        this._perimeter = a + b + c;
+        const p = this._perimeter / 2;
+        
+        this._area = +(Math.sqrt(p * (p - a) * (p - b ) * (p - c)).toFixed(3));
+    }
+    
+    get perimeter() {
+        return this._perimeter || "Ошибка! Треугольник не существует";
+    }
+
+    get area() {
+        return this._area || "Ошибка! Треугольник не существует";
+    }
+
+}
+
+function getTriangle(a, b, c) {
+    try {
+        const tr = new Triangle(a, b, c);        
+        return tr;
+    } catch(e) {
+        return tr;
+    }
+}
